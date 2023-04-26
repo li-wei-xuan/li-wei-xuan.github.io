dc65190762ef4440ff3caa61902bdff842db7231
limou <10554021+li-wei-xuan@user.noreply.gitee.com>
2023-04-26 21:22:32 +0800

    2023-04-26 21:22:29

---
+++ assets/02.gitfenzhiguanli.html-b8a418a0.js
@@ -0,0 +1,15 @@
+import{_ as a,p as n,q as t,w as e,s as i,Y as d}from"./framework-aa5c4115.js";const s={},r=i("p",null,[i("img",{src:"https://img.webvipzone.com/images/git.png",alt:"git"})],-1),c=d(`<h3 id="git-branch" tabindex="-1"><a class="header-anchor" href="#git-branch" aria-hidden="true">#</a> git branch</h3><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>//列出分支
+git branch
+//创建一个分支
+git branch (branchname)
+//删除分支命令：
+git branch -d (branchname)
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><h3 id="git-checkout" tabindex="-1"><a class="header-anchor" href="#git-checkout" aria-hidden="true">#</a> git checkout</h3><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>//切换分支
+git checkout (branch)
+//创建新分支并切换到该分支
+git checkout -b (branchname)
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div><div class="line-number"></div></div></div><h3 id="git-merge" tabindex="-1"><a class="header-anchor" href="#git-merge" aria-hidden="true">#</a> git merge</h3><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>//分支合并
+git merge
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div></div></div><h3 id="git-rebase" tabindex="-1"><a class="header-anchor" href="#git-rebase" aria-hidden="true">#</a> git rebase</h3><div class="language-text line-numbers-mode" data-ext="text"><pre class="language-text"><code>//分支合并
+git rebase
+</code></pre><div class="line-numbers" aria-hidden="true"><div class="line-number"></div><div class="line-number"></div></div></div>`,8);function l(g,h){return n(),t("div",null,[e(" https://www.runoob.com/git/git-basic-operations.html "),r,e(" ![git](/images/git.png) "),c])}const m=a(s,[["render",l],["__file","02.gitfenzhiguanli.html.vue"]]);export{m as default};
